50f03c0e5186531bf76bbbdd376ecb6125936d18
Eldaristy <79899342+Eldaristy@users.noreply.github.com>
2021-04-23 19:30:26 +0300

    fixed sniff.py doc

--- sniff.py
+++ sniff.py
@@ -1,9 +1,12 @@
 from scapy.all import *
 from scapy.sendrecv import sniff 
 
-#seconds - number of seconds to sniff each time.
-#filter_ip - the ip address to filter the sniffing from, should be the server's ip.
-def timed_sniff(seconds: int, filter_ip: str) -> list: #list of bytes where each element is a packet
+'''
+seconds - number of seconds to sniff each time.
+filter_ip - the ip address to filter the sniffing from, should be the server's ip.
+return list - list of bytes where each element is a packet 
+'''
+def timed_sniff(seconds: int, filter_ip: str) -> list: 
     filter = f"not src host {filter_ip}"
     data = sniff(timeout = seconds, filter = filter)
 
